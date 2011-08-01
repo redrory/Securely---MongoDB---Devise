01a26485c4d4af0ede4348e6803a8e22e612ab31
Rory Walker <redrory@gmail.com>
2011-08-01 10:39:07 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3MongoidDevise::Application.config.secret_token = '88574e470b9fcd1809cc04c201dd843be2c855f0ab59b976f0cf96a9d11c24368a28345af11edce0d7423534bf331e6614bbea40c70c0fb1e9356c1dd37c2ccd'
